0e3dc7ea3dd9438bf33129e7ef2ab2c5e9213e6a
Kelly Ng <kellyng19lover@gmail.com>
2024-01-30 09:57:25 -0500

    dg
    dg

--- app.js
+++ app.js
@@ -1,86 +1,89 @@
 const wordArray = [
-    "artistic", "alkaloid","aspiring", 
-    "barefoot", "barterer", "bontanist",
-    "cottager", "catheter", "cesspool",
-    "devoutly", "demeanor", "danseuse",
-    "edifying", "enthrone", "equalize",
-    "feasible", "follicle", "fourthly",
-    "glazzing", "graphics", "gendarme",
-    "herdsman", "huckster", "homeward",
-    "incoming", "immobile", "interact",
-    "jostling", "junkyard", "jewelled",
-    "kilowatt", "kangaroo", "kedgeree",
-    "laminate", "ligneous", "logician",
-    "monoxide", "macaroni", "meekness",
-    "neatness", "nontoxic", "newsroom",
-    "overbold", "ofttimes", "optician",
-    "perforce", "pilchard", "prideful",
-    "quotient", "quackery", "quickset",
-    "reducing", "reaffirm", "rashness",
-    "stimulus", "sneering", "swerving",
-    "turbaned", "twilight", "thoracic",
-    "upstroke", "unionism", "ulterior",
-    "varicose", "violator", "villager",
-    "westard", "wretched", "wiriness",
-    "xenotime", "xenology", "xeronate",
-    "youthful", "youngish", "yeanling",
-    "zucchini", "zincking", "zoneless",
-    ]   
+  "artistic","alkaloid","aspiring",
+  "barefoot","barterer","botanist",
+  "cottager","catheter","cesspool",
+  "devoutly","demeanor","danseuse",
+  "edifying","enthrone","equalize",
+  "feasible","follicle","fourthly",
+  "glazing","graphics","gendarme",
+  "herdsman","huckster","homeward",
+  "incoming","immobile","interact",
+  "jostling","junkyard","jeweled",
+  "kilowatt","kangaroo","kedgeree",
+  "laminate","ligneous","logician",
+  "monoxide","macaroni","meekness",
+  "neatness","nontoxic","newsroom",
+  "overbold","ofttimes","optician",
+  "perforce","pilchard","prideful",
+  "quotient","quackery","quickset",
+  "reducing","reaffirm","rashness",
+  "stimulus","sneering","swerving",
+  "turbaned","twilight","thoracic",
+  "upstroke","unionism","ulterior",
+  "varicose","violator","villager",
+  "westward","wretched","wiriness",
+  "xenotime","xenology","xeronate",
+  "youthful","youngish","yeanling",
+  "zucchini","zincking","zoneless",
+];
 
-const random = Math.floor(Math.random() * wordArray.length);
-console.log(wordArray[random]);
-
-let x = ("random");
 
 const input = document.querySelector(".wordInput");
-const button1 =  document.querySelector(".btn");
+const button1 = document.querySelector(".btn");
 const button2 = document.querySelector(".btnn");
-const container= document.querySelector(".container");
-const card = document.querySelector(".card")
-
-let inputValue = input.value;
-
-let buttons = document.querySelector(".btn", ".btnn")
+const button3 = document.querySelector(".logButton");
+const feedbackMessage = document.querySelector(".feedback-message");
 
 
-function insert(){
-    button1.addEventListener("click", (e) => {
-    e.preventDefault();
-    input.value="";
-    document.querySelector("h3").textContent = (wordArray[random]);
-    })
-  }
+let currentRandomWord = getRandomWord();
 
-// setTimeout(function(){
-//     console.log("Hello World");
-// }, 2000);
 
-function submit(){
-    button2.addEventListener("click", () =>{
-    input.value="";
-    document.querySelector("h3").textContent ="";
-
-    })
+function getRandomWord() {
+  const randomIndex = Math.floor(Math.random() * wordArray.length);
+  return wordArray[randomIndex];
 }
 
-function check(){
-  button2.addEventListener("submit", () => {
-  if ((input.value) == (wordArray[random])) {
-    console.log("correct")}
-  else {
-    console.log("incorrect")
-}})}
+
+function insert() {
+  button1.addEventListener("click", (e) => {
+    e.preventDefault();
+    input.value = "";
+    currentRandomWord = getRandomWord();
+    document.querySelector("h3").textContent = currentRandomWord;
+    feedbackMessage.textContent = "";
+    setTimeout(() => {
+      document.querySelector("h3").textContent = "";
+    }, 3000);
+  });
+}
 
 
-  submit()
-  insert()
-  check()
+function submit() {
+  button2.addEventListener("click", (e) => {
+    e.preventDefault();
+    const inputValue = input.value.trim();
+    if (inputValue === currentRandomWord) {
+      feedbackMessage.textContent = "Correct";
+    } else {
+      feedbackMessage.textContent = "Incorrect";
+    }
+    input.value = "";
+  });
+}
 
-//   DOMSelectors.container.insertAdjacentHTML(
-//     "afterbegin",
-//     `<div class= "card" 
-//     <h3 class="wordd"></h3>
-// </div>`
-//   )
+
+function logWords() {
+  for (let i = 0; i < wordArray.length; i++) {
+    console.log("Word:", wordArray[i]);
+  }
+}
 
 
+button3.addEventListener("click", (e) => {
+  e.preventDefault()
+    logWords();
+});
+
+
+insert();
+submit();
